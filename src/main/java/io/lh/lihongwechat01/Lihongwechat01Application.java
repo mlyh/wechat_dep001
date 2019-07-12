4c2b0513ee923fe338cde551a73c9a2cf60c5170
lihong <1741629633@qq.com>
2019-07-12 17:22:26 +0800

    add first init

---
+++ src/main/java/io/lh/lihongwechat01/Lihongwechat01Application.java
@@ -0,0 +1,13 @@
+package io.lh.lihongwechat01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lihongwechat01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lihongwechat01Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/lh/lihongwechat01/Lihongwechat01ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.lh.lihongwechat01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lihongwechat01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
